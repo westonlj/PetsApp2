20c4d60e5935337fd2e86e6bb1fc3210f1a55c87
Liam Weston <lweston@ncees.org>
2022-05-12 09:57:04 -0400

    Query The DB
    Set the function getPets to query the database and replaced the code in the index file.
    
    Header.php did not have a functioning path to the contact page. Added path and tested the functionality.

--- petsApp/lib/functions.php
+++ petsApp/lib/functions.php
@@ -1,9 +1,14 @@
 <?php
 
+// The following string contains our locally host db 'pet_data' hosted on localhost
+// 'root' and 'null' are where our username and password to the DB would go. 
+// accessing all pets from the pets table
+// fetch all returns a numeric array from a result set (the SQL query below)
 function get_pets()
 {
-    $petsJson = file_get_contents('data/pets.json');
-    $pets = json_decode($petsJson, true);
+    $pdo = new PDO('mysql:dbname=pet_data;host=localhost', 'root', null);
+    $result = $pdo->query('SELECT * FROM pets'); 
+    $pets = $result->fetchAll(); 
 
     return $pets;
 }
